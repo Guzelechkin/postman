66d72b412362f8913f62fc3538f036758b866819
Guzelechkin <cot-oc-pb@mail.ru>
2024-02-21 16:31:58 +0500

    initial commit1

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldCheckoutPostmanEcho() {
+        String text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("He")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text));
+        ;
+    }
+}
